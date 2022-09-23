6439d3370d9125a62cc894305328acf34f7b348f
Kipkemoi <kipkemoilvs@gmail.com>
2022-09-23 10:46:09 +0300

    feat: ppage structure logic #1

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-document.querySelector("#open-popup").addEventListener("click",function(){
-    document.querySelector(".popup").classList.add("active");
-  });
-  
-  document.querySelector(".popup .popup-container .popup-close").addEventListener("click",function(){
-    document.querySelector(".popup").classList.remove("active");
-  });
+const modal = document.querySelector(".modal");
+const trigger = document.querySelector(".trigger");
+const closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
